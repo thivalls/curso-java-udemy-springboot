3985809fae076256bd0cc78653bb477b3d9a123f
valls <olavalls@gmail.com>
2021-06-03 23:01:05 -0300

    Criando seeds e metodos lista all, list one and store new

--- src/main/java/com/udemy/spring/User/UserController.java
+++ src/main/java/com/udemy/spring/User/UserController.java
@@ -1,11 +1,14 @@
 package com.udemy.spring.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("users")
@@ -14,12 +17,18 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping
-    public String index() {
-        List<User> userControllers = userRepository.findAll();
-        if(userControllers.size() == 0) {
-            return "Nenhum usuário cadastrado no banco de dados";
+    public ResponseEntity<List<User>> index() {
+        List<User> users = userRepository.findAll();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> index(@PathVariable Long id) throws Exception {
+        Optional<User> user = userRepository.findById(id);
+        if(user.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário não encontrado");
         }
-        return userControllers.toString();
+        return ResponseEntity.ok(user.get());
     }
 
     @PostMapping
--- src/main/java/com/udemy/spring/config/DbSeedConfig.java
+++ src/main/java/com/udemy/spring/config/DbSeedConfig.java
@@ -3,30 +3,23 @@ package com.udemy.spring.config;
 import com.udemy.spring.User.User;
 import com.udemy.spring.User.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @Profile("test")
-public class DbSeedConfig implements Runnable {
+public class DbSeedConfig implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
     @Override
-    public void run() {
-        String[] names = new String[] { "Thiago", "Amanda", "Theo" };
-        String[] emails = new String[] { "thiago@teste.com", "amanda@teste.com", "theo@teste.com" };
-
-        List<User> users = new ArrayList<>();
-
-        for(int i = 0; i <= names.length; i++) {
-            System.out.println(i);
+    public void run(String... args) throws Exception {
+        for(int i=0; i < 10; i++) {
+            User user = new User(null, "Thiago " + (i+1), "user"+(i+1)+"@gmail.com", "(16) 9.9999.9999", "123456");
+            userRepository.save(user);
         }
-
-        User u1 = new User(null, "Thiago", "user@gmail.com", "(16) 9.9999.9999", "123456");
     }
 }
