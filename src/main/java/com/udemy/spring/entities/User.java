36c5c81768884125cd940d6febd05b99c3593032
valls <olavalls@gmail.com>
2021-08-14 00:30:29 -0300

    mapeando classe user com JPA

--- src/main/java/com/udemy/spring/entities/User.java
+++ src/main/java/com/udemy/spring/entities/User.java
@@ -1,13 +1,18 @@
 package com.udemy.spring.entities;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
 @NoArgsConstructor
-@Data
-public class User {
+@AllArgsConstructor
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
